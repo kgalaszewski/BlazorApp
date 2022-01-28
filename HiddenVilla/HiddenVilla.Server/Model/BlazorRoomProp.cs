eb6f2289eed750075de51228d15dee1ca90d0c2e
Karol <kgalaszewski95@gmail.com>
2022-01-28 15:16:29 +0100

    remove demo project

--- HiddenVilla/HiddenVilla.Server/Model/BlazorRoomProp.cs
+++
@@ -1,9 +0,0 @@
-﻿namespace HiddenVilla.Server.Model
-{
-    public class BlazorRoomProp
-    {
-        public int Id { get; set; }
-        public string Name { get; set; }
-        public string Value { get; set; }
-    }
-}
