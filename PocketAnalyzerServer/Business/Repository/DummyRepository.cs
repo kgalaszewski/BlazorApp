7568723e657a61dce9b81de4e34626e7f42e6e74
Karol <kgalaszewski95@gmail.com>
2022-01-30 21:47:29 +0100

    dummy repo created

---
+++ PocketAnalyzerServer/Business/Repository/DummyRepository.cs
@@ -0,0 +1,85 @@
+﻿using DataAccess.Data;
+using DataAccess.Models;
+using Microsoft.EntityFrameworkCore;
+
+namespace Business.Repository;
+
+public class DummyRepository : IDummyRepository
+{
+    private readonly ApplicationDbContext _dbContext;
+
+    public DummyRepository(ApplicationDbContext dbContext)
+    {
+        _dbContext = dbContext;
+    }
+
+    public async Task<Dummy> CreateDummyAsync(Dummy dummy)
+    {
+        try
+        {
+            var createdDummy = (await _dbContext.Dummies.AddAsync(dummy)).Entity;
+            await _dbContext.SaveChangesAsync();
+            return createdDummy;
+        }
+        catch (Exception ex)
+        {
+            throw new Exception($"ex msg : {ex.Message}\nex: {ex}");
+        }
+    }
+
+    public async Task<IEnumerable<Dummy>> GetAllDummiesAsync()
+    {
+        try
+        {
+            return await _dbContext.Dummies.ToListAsync();
+        }
+        catch (Exception ex)
+        {
+            throw new Exception($"ex msg : {ex.Message}\nex: {ex}");
+        }
+    }
+
+    public async Task<Dummy> GetDummyAsync(int id)
+    {
+        try
+        {
+            return await _dbContext.Dummies.FirstOrDefaultAsync(dummy => dummy.Id == id);
+        }
+        catch (Exception ex)
+        {
+            throw new Exception($"ex msg : {ex.Message}\nex: {ex}");
+        }
+    }
+
+    public async Task<Dummy> UpdateDummyAsync(int dummyId, Dummy newDummyState)
+    {
+        try
+        {
+            var dummy = await _dbContext.Dummies.FindAsync(dummyId);
+            dummy.Name = newDummyState.Name;
+            dummy.Value = newDummyState.Value;
+            var updatedDummy = _dbContext.Dummies.Update(dummy).Entity;
+            await _dbContext.SaveChangesAsync();
+            return updatedDummy;
+        }
+        catch (Exception ex)
+        {
+            throw new Exception($"ex msg : {ex.Message}\nex: {ex}");
+        }
+    }
+
+    public async Task<bool> DeleteDummyAsync(int dummyId)
+    {
+        try
+        {
+            var dummy = await _dbContext.Dummies.FindAsync(dummyId);
+            _dbContext.Dummies.Remove(dummy);
+            await _dbContext.SaveChangesAsync();
+            return true;
+        }
+        catch (Exception ex)
+        {
+            throw new Exception($"ex msg : {ex.Message}\nex: {ex}");
+        }
+    }
+}
---
+++ PocketAnalyzerServer/Business/Repository/IDummyRepository.cs
@@ -0,0 +1,12 @@
+﻿using DataAccess.Models;
+
+namespace Business.Repository;
+
+public interface IDummyRepository
+{
+    public Task<Dummy> CreateDummyAsync(Dummy dummy);
+    public Task<IEnumerable<Dummy>> GetAllDummiesAsync();
+    public Task<Dummy> GetDummyAsync(int id);
+    public Task<Dummy> UpdateDummyAsync(int dummyId, Dummy newDummyState);
+    public Task<bool> DeleteDummyAsync(int dummyId);
+}
